0260d28c49b94af8be369fd3f70fcbcbc0c5a713
Bllprods <bryanlopesrodrigues15@gmail.com>
2026-01-05 16:16:08 -0300

    Conclusão do Lista ToDo
    Correção de Campos. adição da funcionalidade de adicionar novos campos, correção no cadastro de lista, e padronização de saida

--- third/listaToDo.lua
+++ third/listaToDo.lua
@@ -1,24 +1,72 @@
 -- TO-DO-LIST
 
 function Nova()
+    local Lista = {};
     print("Criar nova anotacao:")
-    
+    print("Para tirar um campo de sua Lista digite 3 nele")
     ::reeCad::
 
-    print("Titulo: ")
-    Lista["titulo"] = io.read()
+    for i, Campo in ipairs(Campos) do
+        local Invalido = false
+        ::nextMark::
+        if Campo == "prioridade" then
+            ::InsPrio::
+            if not Invalido then
+                print(Campo .. ": (V = alta | M = normal  | B = baixa)")
+                local Valor = string.lower(io.read())
+                if Valor == "3" then
+                    print("Campo ".. Campo .." Retirado")
+                    goto nextMark
+                elseif Valor ~= "m" and Valor ~= "v" and Valor ~= "b" then
+                    Invalido = true;
+                    goto InsPrio
+                else
+                    Lista[Campo] = Valor
+                end
+            elseif Invalido then
+                print("Valor invalido para " .. Campo .. "!")
+                Invalido = false
+                goto InsPrio
+            end
 
-    print("Data do Compromisso: ")
-    Lista["data"] = io.read()
+        elseif Campo == "data" then
+            ::dataMark::
+            print(Campo .. ": ")
+            local Data = io.read()
+            if Data == "3" then
+                print("Campo ".. Campo .." Retirado")
+                goto nextMark
+            else
+                local dia, mes, ano = Data:match("^(%d%d)/(%d%d)/(%d%d%d%d)$") -- validar formato
+                if dia and mes and ano then
+                    print("Data valida:" .. dia .. mes .. ano)
+                    Lista[Campo] = Data
+                else
+                    print("Formato invalido! Use dd/mm/aaaa")
+                    goto dataMark
+                end
+            end
 
-    print("Conteudo: ")
-    Lista["conteudo"] = io.read()
-
-    print("prioridade: (V = alta | M = normal  | B = baixa")
-    Lista["prioridade"] = string.lower(io.read())
-
-    print("Autor(opcional): ")
-    Lista["autor"] = io.read()
+        else
+            ::novaMark::
+            print(Campo .. ": ")
+            local dado = io.read()
+            if dado == "" then
+                goto novaMark
+            elseif dado == "3" then
+                if Campo == "titulo" or Campo == "conteudo" then
+                    print("este campo n pode ser retirado!")
+                    goto novaMark
+                else
+                    print("Campo ".. Campo .." Retirado")
+                    goto nextMark
+                end
+                
+            else
+                Lista[Campo] = dado
+            end
+        end
+    end
 
     print("confirme os dados: ")
     for chave, valor in pairs(Lista) do
@@ -33,47 +81,115 @@ function Nova()
     end
     io.write("confirmar? 0 para voltar, 1 para continuar: ")
     local conf = io.read("n")
-    io.read("*l")
+    io.read("*l")  -- receber msg vazia pós-numero
 
     if conf == 1 then
         -- https://www.lua.org/manual/5.4/manual.html#6.6
-        table.insert(Listas, Lista)
+        table.insert(Listas, Lista)-- inserir a nova lista no menu
         return true
     else
-        goto reeCad
+        print("Abortar Cadastro? (1 para sim, qualquer outro para nao)")
+        local Confs = io.read("n")
+        io.read("*l") -- limpeza
+        if Confs == 1 then
+            return
+        else
+            goto reeCad
+        end
     end
 end
 function Alt()
+    local indice
+    local item
+    local valido, encontrado = false, false
+    ::Alt::
     if #Listas < 1 then
         print("sem Listas..")
         return
     end
-    print("digite o titulo da lista que pretende alterar")
-    local titulo = io.read()
-    -- ipairs garante a sequencia crescente, o pairs é variavel
+    -- ipairs percorre índices numéricos em ordem crescente e para no primeiro nil
+    -- pairs percorre todas as chaves (nomeadas ou numéricas), sem garantir ordem
+    for i, itens in ipairs(Listas) do
+        print(i .. ": " .. itens.titulo);
+    end
 
-    for i, itens in pairs(Listas) do
-        if itens.titulo == titulo then
+    print("digite o titulo da lista que pretende alterar, ou 1 para abortar")
+    local titulo = string.lower(io.read())
+    if titulo == "1" then
+        return
+    end
+    for i, itens in ipairs(Listas) do
+        if string.lower(itens.titulo) == titulo then
             indice = i
             item = itens
+            encontrado = true
+            break
         end
     end
-
-    for linha, valor in pairs(item) do
-        print(linha .. ": " .. valor)
-    end
-
-    print("qual campo deseja alterar?")
-    io.write("Opções: Titulo, data, conteudo, prioridade e autor:")
-    local dado = string.lower(io.read())
-
-    if dado ~= "id" and dado ~= nil then
-        Listas[indice][dado] = io.read()
-        print("Campo '" .. dado .. "' alterado com sucesso!")
-    elseif dado == "id" then
-        print("este dado não pode ser alterado")
+    if not encontrado then
+        print("Lista nao encontrada")
+        goto Alt
     else
-        print("campo invalido")
+        for linha, valor in pairs(item) do
+            print(linha .. ": " .. valor)
+        end
+
+        print("Qual campo deseja alterar?")
+
+        local listaCampos = table.concat(Campos, ", ")
+        print("Opcoes: " .. listaCampos .. ":")
+        local dado = string.lower(io.read())
+
+        for i, itens in pairs(Campos) do
+            if itens == dado then
+                valido = true
+            end
+        end
+        if valido then
+            ::altdataMark::
+            print("Alterando " .. dado .. "..")
+            if dado == "data" then
+                local Data = io.read()
+                local dia, mes, ano = Data:match("^(%d%d)/(%d%d)/(%d%d%d%d)$") -- validar formato
+                if dia and mes and ano then
+                    print("Data valida:" .. dia .. mes .. ano)
+                    Listas[indice][dado] = Data
+                else
+                    print("Formato invalido! Use dd/mm/aaaa")
+                    goto altdataMark
+                end
+                print("Campo '" .. dado .. "' alterado com sucesso!")
+                Listas[indice]["alterado em"] = os.date("%d/%m/%Y, %H:%M")
+            elseif dado == "prioridade" then
+                local Invalido = false
+                ::InsPrio::
+                if not Invalido then
+                    print(dado .. ": (V = alta | M = normal  | B = baixa)")
+                    local Valor = string.lower(io.read())
+                    if Valor ~= "m" and Valor ~= "v" and Valor ~= "b" then
+                        Invalido = true;
+                        goto InsPrio
+                    else
+                        Listas[indice][dado] = Valor
+                        print("Campo '" .. dado .. "' alterado com sucesso!")
+                        Listas[indice]["alterado em"] = os.date("%d/%m/%Y, %H:%M")
+                    end
+                elseif Invalido then
+                    print("Valor invalido para " .. dado .. "!")
+                    Invalido = false
+                    goto InsPrio
+                end
+            else
+                Listas[indice][dado] = io.read()
+                print("Campo '" .. dado .. "' alterado com sucesso!")
+                Listas[indice]["alterado em"] = os.date("%d/%m/%Y, %H:%M")
+            end
+        elseif dado == "id" then
+            print("este dado não pode ser alterado")
+        else
+            print("campo invalido")
+            goto Alt
+        end
     end
 end
 function Exc()
@@ -82,27 +198,52 @@ function Exc()
         print("sem Listas..")
         return
     end
-    io.write("Digite o titulo da lista a ser excluida: ")
-    local titulo = io.read()
+    for i, itens in ipairs(Listas) do
+        print(i .. ": " .. itens.titulo);
+    end
+
+    io.write("Digite o titulo da lista a ser excluida, ou 1 para Abortar: ")
+    local titulo = string.lower(io.read())
+
+    if titulo == "1" then
+        return
+    end
     for i, valor in ipairs(Listas) do
-        if valor.titulo == titulo then
+        if string.lower(valor.titulo) == titulo then
             table.remove(Listas, i)
             break
         end
     end
 end
 function Atribuir()
+    ::Atri::
     print("atribuir novos valores a lista: ")
-    io.write("Qual o nome do novo valor? ")
-    newValor = io.read()
-    
+    print("Qual o nome do novo valor? Ou clique 1 para abortar ")
+    local newValor = string.lower(io.read())
+    if newValor == "1" then
+        return
+    end
+    print("Confirmar adicao do campo " .. newValor .. "? (1 para sim, 2 para nao)")
+    local respValor = io.read()
+    if respValor == "1" then
+        table.insert(Campos, newValor)
+    else
+        print("adicionar outro campo ou sair? (0 para sair ou 1 para adicionar)")
+        local resp = io.read("n")
+        io.read("*l") -- limpeza
+        if resp == 1 then
+            goto Atri
+        else
+            return
+        end
+    end
 end
 function Cons()
     print("consultando Listas ... ...")
     ::reCalc::
-    print("filtros: ordem de criação(1) || titulo(2) || prioridade(3)")
-    local response = tonumber(io.read())
-    
+    print("filtros: ordem de criacao(1) || titulo(2) || prioridade(3) || 0 para sair")
+    local response = tonumber(io.read("n"))
+    io.read("*l") -- limpeza
 
     -- insere o valor em outra para n alterar a original
     local listaExibida = {}
@@ -122,29 +263,35 @@ function Cons()
         table.sort(listaExibida, function (a, b) --func. interna comparação
             return priValor[a.prioridade] < priValor[b.prioridade]-- ordem prioridade
         end)
+    elseif response == 0 then
+        return
     else
-        print("Opção invalida, re-calculando ...")
+        print("Opcao invalida, re-calculando ...")
         goto reCalc
     end
     if #listaExibida ~= 0 then
-        for chave, valor in ipairs(listaExibida) do -- ipairs para respeitar o sort
+        for chave, item in ipairs(listaExibida) do -- ipairs para respeitar o sort
             print(chave)
-            for v1, valor in pairs(valor) do
-                if v1 == "prioridade" then
-                    if valor == "v" then
-                        valor = "Alta"
-                    elseif valor == "m" then
-                        valor = "Média"
-                    elseif valor == "b" then
-                        valor = "Baixa"
+            for _, campo in ipairs(Campos) do
+                if item[campo] then
+                    local valor = item[campo]
+                    if campo == "prioridade" then
+                        if valor == "v" then
+                            valor = "Alta"
+                        elseif valor == "m" then
+                            valor = "Media"
+                        elseif valor == "b" then
+                            valor = "Baixa"
+                        end
                     end
+                    print(campo .. ": " .. valor)
                 end
-                print(v1 .. ": " .. valor)
             end
         end
     else
         print("Sem listas..\n 0 para voltar")
         if io.read("n") == 0 then
+            io.read("*l") -- limpeza
             return true
         end
     end
@@ -152,23 +299,14 @@ end
 
 print("Gerenciador de To-Do-List:")
 Listas = {}
-Lista = {
-    
+Campos = {
+    "titulo",
+    "data",
+    "conteudo",
+    "prioridade",
+    "autor"
 }
-print("Titulo: ")
-    Lista["titulo"] = io.read()
 
-    print("Data do Compromisso: ")
-    Lista["data"] = io.read()
-
-    print("Conteudo: ")
-    Lista["conteudo"] = io.read()
-
-    print("prioridade: (V = alta | M = normal  | B = baixa")
-    Lista["prioridade"] = string.lower(io.read())
-
-    print("Autor(opcional): ")
-    Lista["autor"] = io.read()
 while true do
     print("Acoes:\n Nova || Alterar || Excluir || Atribuir a || Consultar\n digite 0 para sair")
     io.write("Digite acao desejada: ")
@@ -191,7 +329,7 @@ while true do
         elseif request == "0" then
             dofile("select.lua")
         else
-            goto rotuloReiniciar
+            print("acao invalida")
         end
     end
     -- rotulo para pular em caso de falha
